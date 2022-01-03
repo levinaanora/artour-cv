bbb271b8a2aadf56b85b8917caef53ed04f3caea
levinaanora <levinaanora22@gmail.com>
2022-01-03 20:06:37 +0700

    modified host

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ def send_uploaded_file(filename=''):
     return send_from_directory(app.config["IMAGE_UPLOADS"], filename)
 
 if __name__ == "__main__":
-    app.run(debug = True, use_reloader = True)
+    app.run(debug = True, use_reloader = True, host = "0.0.0.0")
